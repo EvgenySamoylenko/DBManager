d3530c2ec0736a3d8743536ce8d9dde94d99a1e8
EvgenySamoylenko <genyasamoilenko@gmail.com>
2021-04-07 16:35:46 +0300

    Add files via upload

---
+++ DBManager_source/DBConnMode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBManager
+{
+    class DBConnMode
+    {
+        public static string GetConnectionString(string srvName, string dbName, string userName, string pwd, bool IsWinAuth)
+        {
+          string initConnStr;
+
+          if(IsWinAuth == true)
+          {
+            return initConnStr = $"Data Source={srvName};Initial Catalog={dbName};User ID={userName};Password={pwd};Integrated Security={IsWinAuth};";
+          }
+          else
+          {
+            return initConnStr = $"Data Source={srvName};Initial Catalog={dbName};Integrated Security=SSPI;";
+          }
+        }
+    }
+}
